1308b9a8a4594cf1b6d60aa3fff72e7ab41d522e
Irsyadcad <irsyad.taqiuddin@ui.ac.id>
2023-05-13 20:55:53 +0700

    Add authentication ui (login and register)

---
+++ app/src/main/java/com/example/bangkitandroid/ui/authentication/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bangkitandroid.ui.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bangkitandroid.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
