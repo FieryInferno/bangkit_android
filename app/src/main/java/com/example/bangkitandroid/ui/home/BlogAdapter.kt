4d437637a20d0c658414de384f57880a9d026263
cynthiamutiara01 <cynthia.mutiara01@ui.ac.id>
2023-05-14 19:09:16 +0700

    Add adapter for history RecyclerView

---
+++ app/src/main/java/com/example/bangkitandroid/ui/home/BlogAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.bangkitandroid.ui.home
+
+class BlogAdapter {
+}
--- app/src/main/java/com/example/bangkitandroid/ui/home/HomeActivityLogged.kt
+++ app/src/main/java/com/example/bangkitandroid/ui/home/HomeActivityLogged.kt
@@ -2,11 +2,15 @@ package com.example.bangkitandroid.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bangkitandroid.R
 import com.example.bangkitandroid.databinding.ActivityHomeLoggedBinding
+import com.example.bangkitandroid.domain.entities.Disease
+import com.example.bangkitandroid.service.DummyData
 
 class HomeActivityLogged : AppCompatActivity() {
     private lateinit var binding: ActivityHomeLoggedBinding
+    private lateinit var diseases: List<Disease>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,12 +19,37 @@ class HomeActivityLogged : AppCompatActivity() {
 
         supportActionBar?.hide()
 
+        getData()
         setupView()
         setupBottomNavigationView()
     }
 
+    private fun getData(){
+        diseases = DummyData().getHistoryDiseases()
+    }
+
     private fun setupView() {
-        // conditional view, if not logged intent to home not logged
+        // conditional view here, if not logged intent to home not logged
+
+        binding.seeAllTv.setOnClickListener {
+            // intent to disease history page
+        }
+
+        val historyAdapter = HistoryAdapter(diseases)
+        historyAdapter.setOnItemTapCallback(object : HistoryAdapter.OnItemTapCallback{
+            override fun onItemTap(data: Disease) {
+                // intent to disease detail
+            }
+        })
+
+        binding.apply {
+            historyRv.layoutManager = LinearLayoutManager(
+                this@HomeActivityLogged,
+                LinearLayoutManager.HORIZONTAL,
+                false
+            )
+            historyRv.adapter = historyAdapter
+        }
     }
 
     private fun setupBottomNavigationView() {
