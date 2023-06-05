fdda23f4b5cf6478ef30754f65afaec08d5979dd
Yudha Haris Purnama <yudha@clade.ventures>
2023-06-05 14:02:13 +0700

    fix(disease): make appbar backbutton riwayat centered

--- app/src/main/java/com/example/bangkitandroid/ui/disease/DiseaseHistoryActivity.kt
+++ app/src/main/java/com/example/bangkitandroid/ui/disease/DiseaseHistoryActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.paging.LoadState
@@ -60,7 +61,8 @@ class DiseaseHistoryActivity : AppCompatActivity() {
                     Row(
                         modifier = Modifier
                             .padding(top = 24.dp, bottom = 16.dp)
-                            .padding(horizontal = 16.dp)
+                            .padding(horizontal = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically
 
                     ) {
                         Image(
